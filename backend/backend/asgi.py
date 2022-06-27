45478822f6cead339a8a306a2049e15fe60cbab5
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-06-27 15:11:20 +0530

    asgi updated to handle channels authentication

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,14 +1,45 @@
 """
 ASGI config for backend project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+from channels.routing import (
+    URLRouter,
+    ProtocolTypeRouter,
+    get_default_application,
+)
 from django.core.asgi import get_asgi_application
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-application = get_asgi_application()
+asgi_app = get_asgi_application()
+
+from dotenv import load_dotenv
+from django.urls import re_path
+from django.conf import settings
+from .token_auth import JWTAuthMiddleware
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
+# from sequences.api.consumer import *
+
+load_dotenv(settings.BASE_DIR / ".env")
+
+application = ProtocolTypeRouter(
+    {
+        "http": asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            JWTAuthMiddleware(
+                URLRouter(
+                    [
+                        # re_path(os.getenv('BASE_URL'), URLRouter([
+                        # re_path(r'^wsa/backend/$', BackendConsumer.as_asgi(), name='backend-consumer'),
+                        # re_path(r'^wsa/frontend/$', FrontendConsumer.as_asgi(), name='frontend-consumer'),
+                        # ]))
+                    ]
+                )
+            )
+        ),
+    }
+)
