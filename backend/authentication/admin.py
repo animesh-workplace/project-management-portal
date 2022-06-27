e74e02d9082021d8b1a9892ec7927e58f23c0ac4
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-06-27 18:49:01 +0530

    poster error fixed

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -6,16 +6,30 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 class AccountAdmin(UserAdmin):
     list_display = (
-        'username', 'email', 'user_type', 'is_active', 'is_staff', 'is_superuser',
+        "username",
+        "email",
+        "user_type",
+        "is_active",
+        "is_staff",
+        "is_superuser",
     )
     fieldsets = (
         *UserAdmin.fieldsets,
         (
-            'Additional Fields',
+            "Additional Fields",
             {
-                'fields': (
-                    'user_type', 'login_history', 'avatar', 'poster', 'birth_date', 'address',
-                    'city', 'state', 'pin_code', 'institute', 'job_profile', 'bio',
+                "fields": (
+                    "user_type",
+                    "login_history",
+                    "avatar",
+                    "birth_date",
+                    "address",
+                    "city",
+                    "state",
+                    "pin_code",
+                    "institute",
+                    "job_profile",
+                    "bio",
                 ),
             },
         ),
@@ -23,9 +37,9 @@ class AccountAdmin(UserAdmin):
 
 
 class MailTokenAdmin(admin.ModelAdmin):
-    list_display = ('key', 'user', 'created', 'token_type')
-    fields = ('user', 'token_type')
-    ordering = ('-created',)
+    list_display = ("key", "user", "created", "token_type")
+    fields = ("user", "token_type")
+    ordering = ("-created",)
 
 
 admin.site.register(User, AccountAdmin)
