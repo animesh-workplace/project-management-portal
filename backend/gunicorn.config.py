e214b65529166f113c7c0ae73a3b9bbb3fa37dbd
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-06-29 15:06:49 +0530

    updated server starting instructions

--- backend/gunicorn.config.py
+++ backend/gunicorn.config.py
@@ -6,4 +6,3 @@ worker_class = "uvicorn.workers.UvicornWorker"
 
 def on_starting(server):
     print(f"Project version: {__version__}")
-    print("Starting Server [Config se aayaa hain] [on starting]")
