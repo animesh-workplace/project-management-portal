53b942ee824fc920903ba1d31a15d246c4ddbe28
BhupathiSampath <samastha849@gmail.com>
2022-06-29 18:52:17 +0530

    This file allows admin portal to show dynamic projects details(sample identifier tables).

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from projects.models import Projectname, ModelSchema
 
 # Register your models here.
+models = Projectname.objects.all()
+for model in models:
+    reg_model = ModelSchema.objects.get(name=model.modelname).as_model()
+    admin.site.register(reg_model)
