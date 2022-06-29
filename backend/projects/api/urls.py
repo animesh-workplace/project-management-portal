9b777ad6265a44f283a0ad948a4a1a3f70b9953a
BhupathiSampath <samastha849@gmail.com>
2022-06-29 19:16:11 +0530

    contains urls of dynamic projects

---
+++ backend/projects/api/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from projects.api.modules.createmodel import CreateModels
+
+from projects.api.modules.upload_projectsdata import (
+    Getdata,
+    CreateProjectsdata,
+)
+
+urlpatterns = [
+    path("create_model/", CreateModels.as_view(), name="create_model"),
+    path("getmodelsdata/", Getdata.as_view(), name="getmodelsdata"),
+    # path("postdata/", Postdata.as_view(), name="postdata"),
+    # path("getmoduledata/", GetModuledata.as_view(), name="getmoduledata"),
+    path(
+        "uploadprojectsdata/", CreateProjectsdata.as_view(), name="createprojectsdata"
+    ),
+    # path("createmoduledata/", CreateModuledata.as_view(), name="createmoduledata"),
+]
+
+
+# Postdata,
+# GetModuledata,
+# CreateModuledata,
