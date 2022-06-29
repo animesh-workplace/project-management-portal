637c2cc4c908ee66c0dc4fc2d512e94b4c7eb217
BhupathiSampath <samastha849@gmail.com>
2022-06-29 18:49:55 +0530

    This allows the admin portal to show all the dynamic modules

--- backend/modules/admin.py
+++ backend/modules/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from modules.models import Modulename, ModuleModelSchema
 
 # Register your models here.
+module_models = Modulename.objects.all()
+for model in module_models:
+    reg_model = ModuleModelSchema.objects.get(name=model.modulename).as_model()
+    admin.site.register(reg_model)
