30dac81dce92429042fc2b82c38055f28b157e6f
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-06-30 14:41:16 +0530

    renamed url with better name scheme

--- backend/projects/api/urls.py
+++ backend/projects/api/urls.py
@@ -1,18 +1,16 @@
 from django.urls import path
-from .modules.create import CreateSampleIdentifier
-from .modules.upload_projectsdata import (
-    Getdata,
-    CreateProjectsdata,
-)
+from .modules.create import CreateView
+
+# from .modules.upload_projectsdata import (
+#     Getdata,
+#     CreateProjectsdata,
+# )
 
 urlpatterns = [
-    path(
-        "create/",
-        CreateSampleIdentifier.as_view(),
-        name="create-sample-identifier-api",
-    ),
-    path("getmodelsdata/", Getdata.as_view(), name="getmodelsdata"),
-    path(
-        "uploadprojectsdata/", CreateProjectsdata.as_view(), name="createprojectsdata"
-    ),
+    path("create/", CreateView.as_view(), name="create-api"),
+    #
+    # path("getmodelsdata/", Getdata.as_view(), name="getmodelsdata"),
+    # path(
+    #     "uploadprojectsdata/", CreateProjectsdata.as_view(), name="createprojectsdata"
+    # ),
 ]
