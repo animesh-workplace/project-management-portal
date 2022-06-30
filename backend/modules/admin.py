2f1d094eaf13d570b5440e077c0888787ba40ba4
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-06-30 14:39:17 +0530

    Causing error because the table is not created and the request for table comes before the creation

--- backend/modules/admin.py
+++ backend/modules/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from modules.models import Modulename, ModuleModelSchema
 
-# Register your models here.
-module_models = Modulename.objects.all()
-for model in module_models:
-    reg_model = ModuleModelSchema.objects.get(name=model.modulename).as_model()
-    admin.site.register(reg_model)
+# from modules.models import Modulename, ModuleModelSchema
+
+# # Register your models here.
+# module_models = Modulename.objects.all()
+# for model in module_models:
+#     reg_model = ModuleModelSchema.objects.get(name=model.modulename).as_model()
+#     admin.site.register(reg_model)
