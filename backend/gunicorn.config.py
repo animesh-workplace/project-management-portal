e55c9edc60c4196a52d1309b2d13321ebf4d251b
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-06-30 17:34:58 +0530

    quotes fixed

--- backend/gunicorn.config.py
+++ backend/gunicorn.config.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 from backend.__init__ import __version__
 
 load_dotenv(".env")
-bind = f"{os.getenv("BASE_HOST")}:{os.getenv("BASE_PORT")}"
+bind = f"{os.getenv('BASE_HOST')}:{os.getenv('BASE_PORT')}"
 worker_class = "uvicorn.workers.UvicornWorker"
+
 
 def on_starting(server):
     print(f"Project version: {__version__}")
