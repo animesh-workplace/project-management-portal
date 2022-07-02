b6187d574b16061413d29a4984e86981448c4a34
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-07-02 20:00:39 +0530

    added API for metadata creation

---
+++ backend/schema_management/api/modules/metadata.py
@@ -0,0 +1,53 @@
+from rest_framework.response import Response
+from schema_management.models import MetadataHandler
+from rest_framework.permissions import IsAuthenticated
+from authentication.api.utils import create_uniform_response
+from rest_framework import generics, exceptions, serializers, status
+
+
+class CreateMetadataSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    config = serializers.JSONField()
+    project = serializers.CharField()
+
+    class Meta:
+        fields = None
+
+    def validate_name(self, name):
+        qs = ModelSchema.objects
+        model_name = f"{name.lower()}_si"
+        if qs.filter(name__iexact=model_name).exists():
+            raise exceptions.ValidationError("Already exists")
+        return name
+
+    def validate(self, value):
+        name = value.get("name")
+        config = value.get("config")
+        model_name = f"{name.lower()}_si"
+        model_schema = self.create_model(model_name)
+        MetadataHandler.objects.create(name=model_name, config=config)
+        self.register_model(model_schema)
+        return value
+
+
+class CreateMetadataView(generics.GenericAPIView):
+    queryset = MetadataHandler
+    serializer_class = CreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        self.serializer = self.get_serializer(data=request.data)
+        if self.serializer.is_valid():
+            return self.get_response()
+        return Response(
+            create_uniform_response(self.serializer.errors),
+            status=status.HTTP_406_NOT_ACCEPTABLE,
+        )
+
+    def get_response(self):
+        data = {
+            "code": "SUCCESS",
+            "message": "Metadata creation Successful",
+        }
+        response = Response(data, status=status.HTTP_200_OK)
+        return response
