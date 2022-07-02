85b9854b93c4d3a7fdadc33bcd4890367030aa32
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-07-02 20:00:12 +0530

    models updated

--- backend/schema_management/models.py
+++ backend/schema_management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ProjectHandler(models.Model):
+    config = models.JSONField()
+    name = models.CharField(max_length=255)
+    table_name = models.CharField(max_length=255, primary_key=True)
+
+
+class MetadataHandler(models.Model):
+    config = models.JSONField()
+    name = models.CharField(max_length=255)
+    table_name = models.CharField(max_length=255, primary_key=True)
+    project_name = models.ForeignKey(ProjectHandler, on_delete=models.CASCADE)
