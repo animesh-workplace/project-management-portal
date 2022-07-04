86e8cd8226b7b3a739af3ef61507c26e12f32e8e
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-07-04 19:37:29 +0530

    lower is replaced with slugify in metadata

--- backend/schema_management/api/modules/metadata.py
+++ backend/schema_management/api/modules/metadata.py
@@ -53,9 +53,9 @@ class CreateMetadataSerializer(serializers.Serializer):
     @staticmethod
     def get_name(table_type, user, project, metadata=None):
         if table_type == "project":
-            return f"{user}_{project.lower().replace(' ', '_')}_si"
+            return f"{user}_{slugify(project).replace('-', '_')}_si"
         if table_type == "metadata":
-            return f"{user}_{project.lower().replace(' ', '_')}_{metadata.lower().replace(' ', '_')}_metadata"
+            return f"{user}_{slugify(project)..replace('-', '_')}_{slugify(metadata).replace('-', '_')}_metadata"
         raise exceptions.ValidationError("Invalid Type: Get name -> Metadata")
 
     @staticmethod
