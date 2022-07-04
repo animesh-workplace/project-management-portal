815687453d90504bff2351c5b4c40d3626d63e06
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-07-04 20:53:50 +0530

    added ProjectHandler and MetadataHandler to admin panel

--- backend/schema_management/admin.py
+++ backend/schema_management/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MetadataHandler, ProjectHandler
 
-# Register your models here.
+
+# Register your models here
+class ProjectHandlerAdmin(admin.ModelAdmin):
+    list_display = ("name", "table_name")
+    fields = ("name", "table_name", "config")
+
+
+admin.site.register(ProjectHandler, ProjectHandlerAdmin)
+admin.site.register(MetadataHandler)
