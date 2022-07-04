77a295ea64d733c5bb01f845a2d1def8de1445d5
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-07-04 20:54:15 +0530

    added commented for future updates

--- backend/schema_management/models.py
+++ backend/schema_management/models.py
@@ -5,6 +5,7 @@ class ProjectHandler(models.Model):
     config = models.JSONField()
     name = models.CharField(max_length=255)
     table_name = models.CharField(max_length=255, primary_key=True)
+    # Add timestamp for creation and updation
 
 
 class MetadataHandler(models.Model):
@@ -12,3 +13,4 @@ class MetadataHandler(models.Model):
     name = models.CharField(max_length=255)
     table_name = models.CharField(max_length=255, primary_key=True)
     project_name = models.ForeignKey(ProjectHandler, on_delete=models.CASCADE)
+    # Add timestamp for creation and updation
