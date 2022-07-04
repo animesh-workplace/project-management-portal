e15e83c1f8046707052ae76a9fb36a16bba079e0
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-07-04 19:37:53 +0530

    added tasks for handling functions related to the project table

---
+++ backend/project_factory/api/tasks.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import clear_url_caches
+from django.db.utils import IntegrityError
+from importlib import import_module, reload
+from schema_management.models import ProjectHandler
+from project_factory.models import ModelSchema, FieldSchema
+
+
+def create_project_table(table_name, config):
+    # Creating the empty table
+    try:
+        model_schema = ModelSchema.objects.create(name=table_name)
+        return model_schema
+    except IntegrityError:
+        raise serializers.ValidationError(
+            {"Error": "Model already exists"}, code="natural"
+        )
+
+    # Creating fields in the model_schema
+    for item in config:
+        # Might require a try and catch block
+        FieldSchema.objects.create(
+            null=item["null"],
+            name=item["field"],
+            unique=item["unique"],
+            max_length=item["maxlen"],
+            model_schema=model_schema,
+            data_type="character"
+            if (item["datatype"] == "radio")
+            else item["datatype"],
+        )
+
+    # Registering model
+    model = model_schema.as_model()
+    admin.site.register(model)
+    reload(import_module(settings.ROOT_URLCONF))
+    clear_url_caches()
