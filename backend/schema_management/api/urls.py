075b2e673d7053b73e20c886f7299531bde09cb3
BhupathiSampath <samastha849@gmail.com>
2022-07-05 15:57:01 +0530

    Added api urls for uploading project data and metadata

--- backend/schema_management/api/urls.py
+++ backend/schema_management/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from .modules.create_project import CreateProjectView
 from .modules.create_metadata import CreateMetadataView
+from .modules.upload_project_data import UploadProjectView, UploadMetadataView
+
+# from schema_management.api.modules.upload_project_data
 
 urlpatterns = [
     path(
@@ -13,6 +16,30 @@ urlpatterns = [
         "projects/",
         include(
             [path("create/", CreateProjectView.as_view(), name="create-project-api")]
+        ),
+    ),
+    path(
+        "projects/",
+        include(
+            [
+                path(
+                    "upload_project_data/",
+                    UploadProjectView.as_view(),
+                    name="upload-project-data-api",
+                )
+            ]
+        ),
+    ),
+    path(
+        "metadata/",
+        include(
+            [
+                path(
+                    "upload_metadata/",
+                    UploadMetadataView.as_view(),
+                    name="upload-metadata-api",
+                )
+            ]
         ),
     ),
 ]
