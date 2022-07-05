5e58e7a8ea3b8dbc1205db1833174b46c25f7778
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-07-05 11:23:14 +0530

    added function for creating metadata table

--- backend/project_factory/api/tasks.py
+++ backend/project_factory/api/tasks.py
@@ -4,25 +4,57 @@ from django.urls import clear_url_caches
 from django.db.utils import IntegrityError
 from importlib import import_module, reload
 from schema_management.models import ProjectHandler
-from project_factory.models import ModelSchema, FieldSchema
+from ..models import (
+    ProjectSchema,
+    MetadataSchema,
+    ProjectFieldSchema,
+    MetadataFieldSchema,
+)
 
 
 def create_project_table(table_name, config):
     # Creating the empty table
-    model_schema = ModelSchema.objects.create(name=table_name)
+    model_schema = ProjectSchema.objects.create(name=table_name)
 
     # Creating fields in the model_schema
     for item in config:
         # Might require a try and catch block
-        FieldSchema.objects.create(
+        ProjectFieldSchema.objects.create(
             null=item["null"],
-            name=item["field"],
+            name=item["name"],
             unique=item["unique"],
-            max_length=item["maxlen"],
+            max_length=item["max_length"],
             model_schema=model_schema,
             data_type="character"
-            if (item["datatype"] == "radio")
-            else item["datatype"],
+            if (item["data_type"] == "radio")
+            else item["data_type"],
+        )
+
+    # Registering model as a prt of Admin panel
+    model = model_schema.as_model()
+    admin.site.register(model)
+    # Reloading URL path
+    reload(import_module(settings.ROOT_URLCONF))
+    # Clear the URL cache
+    clear_url_caches()
+
+
+def create_metadata_table(table_name, config):
+    # Creating the empty table
+    model_schema = MetadataSchema.objects.create(name=table_name)
+
+    # Creating fields in the model_schema
+    for item in config:
+        # Might require a try and catch block
+        MetadataFieldSchema.objects.create(
+            null=item["null"],
+            name=item["name"],
+            unique=item["unique"],
+            max_length=item["max_length"],
+            model_schema=model_schema,
+            data_type="character"
+            if (item["data_type"] == "radio")
+            else item["data_type"],
         )
 
     # Registering model as a prt of Admin panel
