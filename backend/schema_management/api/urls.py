32529c0e7d5d53f6e14a3fc4e8718c7092be925e
BhupathiSampath <samastha849@gmail.com>
2022-07-05 19:48:35 +0530

    Arranged api urls in proper manner

--- backend/schema_management/api/urls.py
+++ backend/schema_management/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from .modules.create_project import CreateProjectView
 from .modules.create_metadata import CreateMetadataView
-from .modules.upload_project_data import UploadProjectView, UploadMetadataView
+from .modules.upload_project import UploadProjectView
+from .modules.upload_metadata import UploadMetadataView
+from .modules.views import ProjectDetailView
 
 # from schema_management.api.modules.upload_project_data
 
@@ -9,36 +11,22 @@ urlpatterns = [
     path(
         "metadata/",
         include(
-            [path("create/", CreateMetadataView.as_view(), name="create-metadata-api")]
-        ),
-    ),
-    path(
-        "projects/",
-        include(
-            [path("create/", CreateProjectView.as_view(), name="create-project-api")]
-        ),
-    ),
-    path(
-        "projects/",
-        include(
             [
                 path(
-                    "upload_project_data/",
-                    UploadProjectView.as_view(),
-                    name="upload-project-data-api",
-                )
+                    "create/", CreateMetadataView.as_view(), name="create-metadata-api"
+                ),
+                path(
+                    "upload/", UploadMetadataView.as_view(), name="create-metadata-api"
+                ),
             ]
         ),
     ),
     path(
-        "metadata/",
+        "projects/",
         include(
             [
-                path(
-                    "upload_metadata/",
-                    UploadMetadataView.as_view(),
-                    name="upload-metadata-api",
-                )
+                path("create/", CreateProjectView.as_view(), name="create-project-api"),
+                path("upload/", UploadProjectView.as_view(), name="upload-project-api"),
             ]
         ),
     ),
