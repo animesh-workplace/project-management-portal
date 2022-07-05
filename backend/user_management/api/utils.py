d91c0bfdd4b64cacfdc11634233a995a7eb83aba
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-07-05 15:55:59 +0530

    updated uniform response generator

--- backend/user_management/api/utils.py
+++ backend/user_management/api/utils.py
@@ -9,45 +9,52 @@ init(autoreset=True)
 
 
 def import_callable(path_or_callable):
-    if (hasattr(path_or_callable, '__call__')):
+    if hasattr(path_or_callable, "__call__"):
         return path_or_callable
     else:
         assert isinstance(path_or_callable, str)
-        package, attr = path_or_callable.rsplit('.', 1)
+        package, attr = path_or_callable.rsplit(".", 1)
         return getattr(import_module(package), attr)
 
 
-def create_uniform_response(errors):
-    print('this is printed', errors)
-    final_message = []
-    for key, value in errors.items():
-        print(key)
-        if (key not in ['detail', 'Error']):
-            final_message.append(
-                f"{key} - {value[0].replace('.', '') if(isinstance(value, list)) else value.replace('.', '')}"
-            )
-        else:
-            final_message.append(
-                f"{value[0].replace('.', '') if(isinstance(value, list)) else value.replace('.', '')}"
-            )
+def generate_response(title, body):
+    match title:
+        case "non_field_errors":
+            msg_head = ""
+        case _:
+            msg_head = f"{title.capitalize()} :"
+    msg_body = (
+        body[0].replace(".", "") if (isinstance(body, list)) else body.replace(".", "")
+    )
+    message = f"{msg_head} {msg_body}".strip()
+    return message
 
-    return {"message": ', '.join(final_message), "code": "ERROR"}
-    # return {
-    # 	"message": errors,
-    # 	"code": "ERROR"
-    # }
+
+def create_uniform_response(errors):
+    print("this is printed", errors)
+    final_message = []
+    if isinstance(errors, list):
+        for error in errors:
+            for key, value in error.items():
+                final_message.append(generate_response(key, value))
+    else:
+        for key, value in errors.items():
+            final_message.append(generate_response(key, value))
+    return {"message": "\n".join(final_message), "code": "ERROR"}
 
 
 def custom_exception_handler(exc, context):
-    print('exec and context --', exc, context)
+    print("exec and context --", exc, context)
     response = exception_handler(exc, context)
-    if (response):
-        return Response(create_uniform_response(response.data),
-                        status=exc.status_code)
-    print(f'{Fore.YELLOW}\n\nMessage starts here ------------\n\n')
-    print(f'{Fore.RED}'.join(
-        traceback.format_exception(etype=type(exc), value=exc, tb=exc.__traceback__)))
-    print(f'{Fore.YELLOW}\n\nMessage ends here ------------\n\n')
+    if response:
+        return Response(create_uniform_response(response.data), status=exc.status_code)
+    print(f"{Fore.YELLOW}\n\nMessage starts here ------------\n\n")
+    print(
+        f"{Fore.RED}".join(
+            traceback.format_exception(etype=type(exc), value=exc, tb=exc.__traceback__)
+        )
+    )
+    print(f"{Fore.YELLOW}\n\nMessage ends here ------------\n\n")
     message = {
         "code": "ERROR",
         "message": "Error occured, admin has been informed",
@@ -57,8 +64,8 @@ def custom_exception_handler(exc, context):
 
 class OnlyUnAuthenticated(BasePermission):
     """
-       Allows access only to unauthenticated users
+    Allows access only to unauthenticated users
     """
 
     def has_permission(self, request, view):
-        return (not bool(request.user and request.user.is_authenticated))
+        return not bool(request.user and request.user.is_authenticated)
