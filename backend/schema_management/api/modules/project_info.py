3c615050f4bac8db23f934cdba367ce38872b4fe
BhupathiSampath <samastha849@gmail.com>
2022-07-06 14:31:56 +0530

    Added api to get single project information.

---
+++ backend/schema_management/api/modules/project_info.py
@@ -0,0 +1,36 @@
+from django.apps import apps
+from rest_framework.response import Response
+from schema_management.models import ProjectHandler
+from rest_framework.permissions import IsAuthenticated
+from user_management.api.utils import create_uniform_response
+from rest_framework import generics, exceptions, serializers, status
+
+
+class ProjectDetailSerializer(serializers.Serializer):
+    name = serializers.CharField()
+
+    class Meta:
+        fields = "__all__"
+
+    def validate(self, value):
+        name = value.get("name")
+        if ProjectHandler.objects.filter(table_name=name).exists():
+            project_model = self.context["view"].get_queryset()[name.lower()]
+            queryset = project_model.objects.all().values()
+            return queryset
+        raise exceptions.ValidationError(f"{name} is not exists in metadata_model")
+
+
+class ProjectDetailView(generics.GenericAPIView):
+    queryset = apps.get_app_config("table_factory").models
+    serializer_class = ProjectDetailSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        self.serializer = self.get_serializer(data=request.data)
+        if self.serializer.is_valid():
+            return Response(self.serializer.validated_data)
+        return Response(
+            create_uniform_response(self.serializer.errors),
+            status=status.HTTP_406_NOT_ACCEPTABLE,
+        )
