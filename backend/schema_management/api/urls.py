388873efc592050a9b0bf80ac6fe6d838a3f44e2
BhupathiSampath <samastha849@gmail.com>
2022-07-06 14:29:00 +0530

    Added extra urls for meta data info and names, projects info and names

--- backend/schema_management/api/urls.py
+++ backend/schema_management/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from .modules.create_project import CreateProjectView
-from .modules.create_metadata import CreateMetadataView
+from .modules.project_names import ProjectNamesView
+from .modules.project_info import ProjectDetailView
+from .modules.metadata_info import MetadataDetailView
+from .modules.metadata_names import MetadataNamesView
 from .modules.upload_project import UploadProjectView
+from .modules.create_project import CreateProjectView
 from .modules.upload_metadata import UploadMetadataView
-from .modules.views import ProjectDetailView
+from .modules.create_metadata import CreateMetadataView
 
 # from schema_management.api.modules.upload_project_data
 
@@ -18,6 +21,8 @@ urlpatterns = [
                 path(
                     "upload/", UploadMetadataView.as_view(), name="create-metadata-api"
                 ),
+                path("info/", MetadataDetailView.as_view(), name="metadata-info-api"),
+                path("names/", MetadataNamesView.as_view(), name="metadata-names-api"),
             ]
         ),
     ),
@@ -27,6 +32,8 @@ urlpatterns = [
             [
                 path("create/", CreateProjectView.as_view(), name="create-project-api"),
                 path("upload/", UploadProjectView.as_view(), name="upload-project-api"),
+                path("info/", ProjectDetailView.as_view(), name="project-info-api"),
+                path("names/", ProjectNamesView.as_view(), name="project-names-api"),
             ]
         ),
     ),
