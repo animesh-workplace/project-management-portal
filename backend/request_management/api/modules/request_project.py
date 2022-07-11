de14a760b5b8c9299b8bd1025925b7148425fe08
BhupathiSampath <samastha849@gmail.com>
2022-07-11 10:07:29 +0530

    lint

--- backend/request_management/api/modules/request_project.py
+++ backend/request_management/api/modules/request_project.py
@@ -1,10 +1,10 @@
+from user_management.models import User
 from rest_framework.response import Response
 from request_management.models import UserRequest
 from rest_framework.permissions import IsAuthenticated
 from user_management.api.utils import create_uniform_response
 from schema_management.models import MetadataHandler, ProjectHandler
 from rest_framework import generics, exceptions, serializers, status
-from user_management.models import User
 
 
 class UserRequestSerializer(serializers.Serializer):
