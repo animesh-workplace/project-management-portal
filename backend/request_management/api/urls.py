8e2968e86d40852b76c2c4b036fb6d5d109f24f3
BhupathiSampath <samastha849@gmail.com>
2022-07-11 17:54:31 +0530

    Added url to accepting project permissions

--- backend/request_management/api/urls.py
+++ backend/request_management/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .modules.request_project import UserRequestView
+from .modules.accept_project import ProjectPermissionsView
 
 urlpatterns = [
     path("request/", UserRequestView.as_view(), name="request-project-api"),
+    path("accept/", ProjectPermissionsView.as_view(), name="accept-project-api"),
 ]
