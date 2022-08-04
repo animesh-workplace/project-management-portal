06e12b3ad7f39372ecb15756ab02307b16b59eb7
BhupathiSampath <samastha849@gmail.com>
2022-08-04 14:19:52 +0530

    base file for storing apis data

---
+++ frontend/store/base.js
@@ -0,0 +1,126 @@
+import { getField, updateField } from "vuex-map-fields";
+
+export const state = () => ({
+	baseURL: {},
+	projectList: {},
+	projectList_loaded: false,
+
+	name: "",
+	metadataname: "",
+	projectInfo: {},
+	projectInfo_loaded: false,
+
+	metadataList: {},
+	metadataInfo: {},
+
+	projectConfig: {},
+	metadataConfig: [],
+});
+
+export const getters = {
+	getField,
+};
+
+export const mutations = {
+	SET_ProjectList(state, payload) {
+		state.projectList = payload;
+		state.projectList_loaded = true;
+		state.name = state.projectList.projects[0]
+	},
+	SET_ProjectInfo(state, payload) {
+		state.projectInfo = payload;
+		state.projectInfo_loaded = true;
+	},
+	SET_MetadataList(state, payload) {
+		state.metadataList = payload;
+		state.metadataname = state.metadataList.metadata[0]
+	},
+	SET_MetadataInfo(state, payload) {
+		state.metadataInfo = payload;
+	},
+	SET_ProjectConfig(state, payload) {
+		state.projectConfig = payload
+	},
+	SET_MetadataConfig(state, payload) {
+		state.metadataConfig = payload
+	},
+	updateField,
+};
+
+export const actions = {
+	async ProjectList({ commit, dispatch }) {
+		const response = await this.$axios.$post(
+			'http://10.10.6.87/pmp/api/schema/projects/names/'
+		);
+		await commit("SET_ProjectList", response);
+	},
+	async ProjectInfo({ commit, dispatch }, payload) {
+		await dispatch("ProjectList");
+		const response = await this.$axios.$post(
+			'http://10.10.6.87/pmp/api/schema/projects/info/', payload
+		);
+		await commit("SET_ProjectInfo", response);
+	},
+	async MetadataList({ commit, dispatch }, payload) {
+		const response = await this.$axios.$post(
+			'http://10.10.6.87/pmp/api/schema/metadata/names/', payload
+		);
+		await commit("SET_MetadataList", response);
+	},
+	async MetadataInfo({ commit, dispatch, state }, payload) {
+		try {
+			// await dispatch("MetadataList", {project_name: state.name});
+			const response = await this.$axios.$post(
+				'http://10.10.6.87/pmp/api/schema/metadata/info/', payload
+			);
+			await commit("SET_MetadataInfo", response);
+		} catch (err) {
+			// state.projectInfo = {}
+            this.$notification.show(err.response.statusText, Object.values(err.response.data)[0], 'ERROR')
+        }
+	},
+	async CreateSampleIdentifier({ commit, dispatch }, payload) {
+		try {
+            const response = await this.$axios.$post('http://10.10.6.87/pmp/api/schema/projects/create/', payload)
+            this.$notification.show(response.code, response.message, 'SUCCESS')
+        } catch (err) {
+            this.$notification.show(err.response.statusText, Object.values(err.response.data)[0], 'ERROR')
+        }
+	},
+	async CreateMetadata({ commit, dispatch }, payload) {
+		try {
+            const response = await this.$axios.$post('http://10.10.6.87/pmp/api/schema/metadata/create/', payload)
+            this.$notification.show(response.code, response.message, 'SUCCESS')
+        } catch (err) {
+            this.$notification.show(err.response.statusText, Object.values(err.response.data)[0], 'ERROR')
+        }
+	},
+	async ProjectConfig({ commit, dispatch }, payload) {
+		const response = await this.$axios.$post(
+			'http://10.10.6.87/pmp/api/schema/projects/config/', payload
+		);
+		await commit("SET_ProjectConfig", response);
+	},
+	async MetadataConfig({ commit, dispatch }, payload) {
+		const response = await this.$axios.$post(
+			'http://10.10.6.87/pmp/api/schema/metadata/config/', payload
+		);
+		await commit("SET_MetadataConfig", response);
+	},
+	async UploadProject({ commit, dispatch }, payload) {
+		try {
+            const response = await this.$axios.$post('http://10.10.6.87/pmp/api/schema/projects/upload/', payload)
+            this.$notification.show(response.code, response.message, 'SUCCESS')
+        } catch (err) {
+            this.$notification.show(err.response.statusText, Object.values(err.response.data)[0], 'ERROR')
+        }
+	},
+	async UploadMetadata({ commit, dispatch }, payload) {
+		try {
+            const response = await this.$axios.$post('http://10.10.6.87/pmp/api/schema/metadata/upload/', payload)
+            this.$notification.show(response.code, response.message, 'SUCCESS')
+        } catch (err) {
+            this.$notification.show(err.response.statusText, Object.values(err.response.data)[0], 'ERROR')
+        }
+	},
+};
