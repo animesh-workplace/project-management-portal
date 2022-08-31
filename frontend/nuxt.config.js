ab8d4eb37b374797349bca042e24b6b237239695
BhupathiSampath <samastha849@gmail.com>
2022-08-31 10:26:10 +0530

    removed commented lines of duplicated auth

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -35,44 +35,6 @@ export default {
     baseURL: "http://10.10.6.87/pmp/api/user",
   },
 
-  // auth: {
-  //   strategies: {
-  //     local: {
-  //       scheme: "refresh",
-  //       localStorage: {
-  //         prefix: "auth.",
-  //       },
-  //       token: {
-  //         prefix: "access_token.",
-  //         property: "access_token",
-  //         maxAge: 86400,
-  //         type: "Bearer",
-  //       },
-  //       refreshToken: {
-  //         prefix: "refresh_token.",
-  //         property: "refresh_token",
-  //         data: "refresh_token",
-  //         maxAge: 60 * 60 * 24 * 15,
-  //       },
-  //       user: {
-  //         property: "user",
-  //         autoFetch: true,
-  //       },
-  //       endpoints: {
-  //         login: { url: "/login/", method: "post" },
-  //         refresh: { url: "/token/refresh/", method: "post" },
-  //         user: { url: "/user", method: "get" },
-  //         logout: { url: "/logout_view", method: "post" },
-  //       },
-  //       redirect: {
-  //         login: "/login/",
-  //         logout: "/login/",
-  //         index: "/login/",
-  //       },
-  //     },
-  //   },
-  // },
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
